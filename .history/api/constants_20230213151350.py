6523d7adc2cc5a9ae6c4742e8a7d544baea6a16f
unknown <x17project@gmail.com>
2023-02-13 15:15:12 +0100

    Changed the prefixed URL

--- .history/api/constants_20230213151350.py
+++ .history/api/constants_20230213151350.py
@@ -2,6 +2,6 @@ from flask import Flask, Blueprint
 from flask_restx import Api
 
 APP = Flask(__name__)
-BLUEPRINT = Blueprint('api', __name__, url_prefix='/')
+BLUEPRINT = Blueprint('api', __name__)
 API = Api(BLUEPRINT)
 DATABASE = 'devices-kw01.db'
